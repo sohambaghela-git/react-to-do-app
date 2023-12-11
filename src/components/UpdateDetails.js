5db94e7bc754647bdd3c98e810abddce13308d4d
soham <sobaghela@bestpeers.com>
2023-12-11 13:10:40 +0530

    Update style methods icons and indentation

--- src/components/UpdateDetails.js
+++ src/components/UpdateDetails.js
@@ -41,61 +41,61 @@ const UpdateDetails = (props) => {
                 item.id === formData.id ? { ...item, ...formData } : item
             )
             );
-            navigate(`/detail/${formData.id}`);
+            navigate(`/`);
         }
     };
 
     return (
-        <div className="container mt-4">
-            <h2>To Do Form</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="id" className="form-label">ID:</label>
-                    <input
-                    type="text"
-                    className="form-control"
-                    id="id"
-                    name="id"
-                    value={formData.id}
-                    onChange={handleInputChange}
-                    readOnly  // Make the input read-only to prevent user input
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name:</label>
-                    <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="description" className="form-label">Description:</label>
-                    <textarea
-                    className="form-control"
-                    id="description"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleInputChange}
-                    ></textarea>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="date" className="form-label">Date:</label>
-                    <input
-                    type="date"
-                    className="form-control"
-                    id="date"
-                    name="date"
-                    value={formData.date}
-                    onChange={handleInputChange}
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">Update</button>
-            </form>
-        </div>
+      <div className="container mt-4">
+        <h2>To Do Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="id" className="form-label">ID:</label>
+            <input
+            type="text"
+            className="form-control"
+            id="id"
+            name="id"
+            value={formData.id}
+            onChange={handleInputChange}
+            readOnly  // Make the input read-only to prevent user input
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name:</label>
+            <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">Description:</label>
+            <textarea
+            className="form-control"
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+            ></textarea>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="date" className="form-label">Date:</label>
+            <input
+            type="date"
+            className="form-control"
+            id="date"
+            name="date"
+            value={formData.date}
+            onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">Update</button>
+        </form>
+      </div>
     );
 }
 
